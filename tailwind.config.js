eb95166486cc1eef4b7590949caeb24b8b0d270d
Matias Bubello <matt.bubello@gmail.com>
2024-01-10 15:22:24 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Nunito','Inter', ...defaultTheme.fontFamily.sans],
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
