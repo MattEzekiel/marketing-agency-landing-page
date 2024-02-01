9d3de23f6189db11fe2a4769360091373a1d9869
Matias Bubello <matt.bubello@gmail.com>
2024-02-01 15:16:26 -0300

    reverted

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gconsulting.agency',
-  base: 'https://github.com/MattEzekiel/marketing-agency-landing-page.github.io',
   integrations: [tailwind(), sitemap()]
 });
