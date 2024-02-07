0e4d5c2bba08969846b25f2a1d4417663044fb2a
Matias Bubello <matt.bubello@gmail.com>
2024-02-07 16:26:30 -0300

    setting up cases

---
+++ src/helpers/index.js
@@ -0,0 +1,47 @@
+export const successCases = [
+  {
+    title: 'Social Media Strategy and Google Ads',
+    client: 'Club Spa',
+    background: 'case-1',
+  },
+  {
+    title: 'Employer Branding Video',
+    client: 'Real Trends',
+    background: 'case-2',
+  },
+  {
+    title: 'Continuing Medical Education Portal',
+    client: 'Laboratorio',
+    background: 'case-3',
+  },
+  {
+    title: 'Marketing Consulting',
+    client: 'Alma Loft',
+    background: 'case-4',
+  },
+  {
+    title: 'Google Ads',
+    client: 'Ozone Lifestyle & Maria T',
+    background: 'case-5',
+  },
+  {
+    title: 'Google Ads y Estrategia de eMail Marketing',
+    client: 'BAS Indumentaria',
+    background: 'case-6',
+  },
+  {
+    title: 'Consultoría de Marketing',
+    client: 'flowandbliss',
+    background: 'case-7',
+  },
+  {
+    title: 'Real Trends',
+    client: 'Looker Studio Dashboards',
+    background: 'case-8',
+  },
+  {
+    title: 'Estrategia Growth eCommerce',
+    client: 'Walmart',
+    background: 'case-9',
+  },
+];
